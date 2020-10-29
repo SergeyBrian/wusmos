57ca15c5105e085e80677311ece30b41fe03b056
Sergio Brianski <s.brianski@yahoo.com>
2020-10-29 16:52:58 +0300

    Docker is set up

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,4 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port='3000')
+    app.run(debug=True, host='0.0.0.0', port='3000')
