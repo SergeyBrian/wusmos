11a080e379464dfd81df0c4c4c650829e2102054
Sergio Brianski <s.brianski@yahoo.com>
2020-10-29 21:12:05 +0300

    Basic html output

--- Dockerfile
+++ Dockerfile
@@ -19,5 +19,7 @@ RUN adduser -D myuser
 USER myuser
 
 # Run the app.  CMD is required to run on Heroku
-# $PORT is set by Heroku			
+# $PORT is set by Heroku
+
+
 CMD gunicorn --bind 0.0.0.0:$PORT wsgi
--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello world from docker hosted on heroku!'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
