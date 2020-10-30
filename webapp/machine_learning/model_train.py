9edf2c3aaf8bf9a368d6454d447e1e360b3e1049
Sergio Brianski <s.brianski@yahoo.com>
2020-10-30 08:50:46 +0300

    Import data to the model train script

--- webapp/machine_learning/model_train.py
+++ webapp/machine_learning/model_train.py
@@ -1,10 +1,15 @@
-import pandas as ps
+import pandas
 import numpy as np
 import pickle
 
+DIR = ''
+if __name__ != '__main__':
+    DIR = 'machine_learning/'
 
 def new_model():
-    return "HELLO!!!"
+    train = pandas.read_csv(DIR + 'twitter_data_public.csv')
+    # test = train.turncate(after=50000)
+    return 'ok'
 
 
 def start_learning():
