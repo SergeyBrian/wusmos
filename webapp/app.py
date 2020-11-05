65c8b12771b67bda391d1c95f25eb128dd7cd3a1
Sergio Brianski <s.brianski@yahoo.com>
2020-11-05 15:52:39 +0300

    Final result

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,6 @@
 import os
 
-from flask import Flask, render_template
-import machine_learning.machine_learning as mt
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,8 +12,16 @@ def hello():
 
 @app.route('/machinelearning')
 def ml():
-
     return 'ok'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/graph')
+def graph():
+    return render_template('graph.html', res=request.args.get('res'))
 
 
 if __name__ == '__main__':
